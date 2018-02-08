add69c894c01c9abc0436db22ecceb6277911f6e
Riley <rileyjohnson99@hotmail.com>
2018-02-07 22:18:39 -0600

    Init commit

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) { http.ServeFile(c.Writer, c.Request, "./index.html") })
+	r.GET("/static/css/:fi", static.Serve("/static/css", static.LocalFile("static/css/", true)))
+	r.GET("/static/img/:fi", static.Serve("/static/img", static.LocalFile("static/img/", true)))
+	r.GET("/static/js/:fi", static.Serve("/static/js", static.LocalFile("static/js/", true)))
+	r.Run(":5599")
+}
---
+++ static/js/yong.js
@@ -0,0 +1,16 @@
+var v = new Vue({
+    el: '#app',
+    data: function() {
+        return {
+            teachers: [
+                {name: "Yong Wang", courseTitle: "Course Title", courseDescription: "This is the course's description", infoLink: "http://dsu.edu/about-dsu/directory/yong-wang", image: "./static/img/wang-yong.jpg"}
+            ]
+        }
+    },
+    methods: {
+
+    },
+    mounted: function() {
+
+    }
+});
